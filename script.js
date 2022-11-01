3bf011d271d5bce3f26b23c3da97b7f5c764d342
skaichima <skaichima@gmail.com>
2022-11-01 12:41:20 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const input_btn = document.querySelector("#input-btn");
+const options = document.querySelector("#voice")
+let voices = speechSynthesis.getVoices();
+for (i = 0; i < voices.length; i++) {
+    options.innerHTML += `<option value="${voices[i].name}">${voices[i].name}</option>`
+}
+input_btn.addEventListener('click', () =>{
+    const text = document.querySelector("#input-text").value;
+    if(text){
+        let utterance = new SpeechSynthesisUtterance(text);
+        let value = options.value;
+        voices.forEach(voice => {
+            if(value === voice.name) utterance.voice = voice
+        });
+        speechSynthesis.speak(utterance);
+    }
+})
